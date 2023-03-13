942ad7cc2e289f5ecc9c2203ff10ca2962410b78
kuwabarbarac:/Users/nobua/file/tweet/tweetList.py <nobuaki.kuwabara3@gmail.com>
2023-03-13 15:54:09 +0900

    ログイン中の画面でアイコンとemailを表示させることに成功

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>Slack Clone</h1>
+
+    <div class="w-1/5 bg-gray-800 text-white pt-3 px-4">
+      <h1 class="font-semibold text-xl leading-tight">Slack Clone</h1>
+      <Notification />
+      <div>{{ user.email }}</div>
+    </div>
+
     <p>ログイン中</p>
     <div>
        <button class="py-1 px-4 bg-gray-800 text-white rounded" @click="signOut">サインアウト</button>
@@ -11,13 +17,20 @@
 <script>
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import Notification from "../components/icons/Notification";
 
 export default {
+  components: {
+    Notification
+  },
   methods: {
     signOut() {
       firebase.auth().signOut();
       this.$router.push("/signin");
     }
+  },
+  mounted(){
+    this.user = firebase.auth().currentUser;
   }
 };
 </script>
